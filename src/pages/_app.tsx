a8ade16db8a6bc22198b21442f31f276034407b7
0xyshv <writetoyshv@gmail.com>
2024-01-18 13:15:40 +0530

    feat: Wallet setup with connectkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { WagmiConfig, createConfig } from "wagmi";
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || "",
+
+    // Required
+    appName: "DeWork",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co",
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
