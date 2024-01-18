baf9571e65a8113196aca60149b84115b2b2895c
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-19 01:13:51 +0530

    feat: update shadcn configs

--- src/components/client/client-navbar.tsx
+++ src/components/client/client-navbar.tsx
@@ -4,6 +4,15 @@ import * as React from "react";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 import {
   NavigationMenu,
@@ -15,6 +24,7 @@ import {
 import { ConnectKitButton } from "connectkit";
 
 export function ClientNavbar() {
+  const { setTheme } = useTheme();
   return (
     <div className="flex justify-between">
       <NavigationMenu>
@@ -42,7 +52,30 @@ export function ClientNavbar() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <ConnectKitButton showBalance />
+      <div className="flex gap-5">
+        <ConnectKitButton showBalance />
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
