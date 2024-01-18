61a768b0290b66e695f5b80ee8aa9978b4344f80
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-18 13:22:43 +0530

    feat: add shadcn to codebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { ConnectKitButton } from "connectkit";
+import { Button } from "@/components/ui/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <ConnectKitButton />
+      <Button>Click me</Button>
     </>
   );
 }
