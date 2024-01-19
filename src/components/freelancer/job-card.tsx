1e3cd8d77dcce9502ab68c54b6622b65ad13ed41
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-19 23:37:54 +0530

    feat: update Job Card for freelancer

--- src/components/freelancer/job-card.tsx
+++ src/components/freelancer/job-card.tsx
@@ -20,91 +20,73 @@ import {
 } from "lucide-react";
 import SendProposalForm from "./send-proposal-form";
 
-type CardProps = React.ComponentProps<typeof Card>;
-
-export function JobCard({ className, ...props }: CardProps) {
-  const [jobs, setJobs] = useState<Job[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getAllActiveJobs();
-        setJobs(data);
-      } catch (error) {
-        console.error("Error fetching job data:", error);
-      }
-    };
-    fetchData();
-  }, []);
+export function JobCard({ job }: { job: Job }) {
+  // useEffect(() => {}, []);
 
   return (
     <div className="p-8 grid gap-8 ">
-      {jobs.map((job) => (
-        <Card
-          key={job.jobId}
-          className={cn(
-            "bg-opacity-65 shadow-lg dark:bg-purple-300 dark:bg-opacity-15",
-            className
-          )}
-          {...props}
-        >
-          <CardHeader>
-            <CardTitle className="flex justify-between items-center">
-              {job.title}
-              <p className="px-2 py-1 text-white text-sm font-bold rounded-md bg-green-800 dark:text-black dark:bg-white">
-                Job Id : {job.jobId}
-              </p>
-            </CardTitle>
-            <CardDescription>
-              <p>{job.description}</p>
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="flex gap-2 items-center">
-              <Briefcase className="h-5" />
-              <div className="flex gap-4">
-                <p className="text-lg font-thin">Skills Required :</p>
-                {job.tags.map((tag, index) => (
-                  <Badge
-                    key={index}
-                    className="text-sm bg-green-900 dark:bg-purple-500 dark:text-white "
-                  >
-                    {tag}
-                  </Badge>
-                ))}
-              </div>
-            </div>
-            <div className="flex gap-2 items-center">
-              <BadgeDollarSignIcon className="h-5" />
-              <p className="text-lg font-thin">Budget : ${job.budget}</p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <BookUser className="h-5" />
-              <p className="text-lg font-thin">
-                Total Proposals : {job.proposals}
-              </p>
-            </div>
-            <div className="flex gap-2 items-center">
-              <BookUser className="h-5" />
-              <p className="text-lg font-thin">
-                Creator :{" "}
-                <span className="text-blue-600 font-normal cursor-pointer">
-                  {job.createdBy}
-                </span>
-              </p>
-            </div>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <div className="flex gap-2 items-center">
-              <Calendar className="h-5" />
-              <p className="font-semibold">Posted Date : {job.createdAt}</p>
-            </div>
+      <Card
+        key={job.jobId}
+        className={cn(
+          "bg-opacity-65 shadow-lg dark:bg-purple-300 dark:bg-opacity-15"
+        )}
+      >
+        <CardHeader>
+          <CardTitle className="flex justify-between items-center">
+            {job.title}
+            <p className="px-2 py-1 text-white text-sm font-bold rounded-md bg-green-800 dark:text-black dark:bg-white">
+              Job Id : {job.jobId}
+            </p>
+          </CardTitle>
+          <CardDescription>
+            <p>{job.description}</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="flex gap-2 items-center">
+            <Briefcase className="h-5" />
             <div className="flex gap-4">
-              <SendProposalForm />
+              <p className="text-lg font-thin">Skills Required :</p>
+              {job.tags.map((tag, index) => (
+                <Badge
+                  key={index}
+                  className="text-sm bg-green-900 dark:bg-purple-500 dark:text-white "
+                >
+                  {tag}
+                </Badge>
+              ))}
             </div>
-          </CardFooter>
-        </Card>
-      ))}
+          </div>
+          <div className="flex gap-2 items-center">
+            <BadgeDollarSignIcon className="h-5" />
+            <p className="text-lg font-thin">Budget : ${job.budget}</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <BookUser className="h-5" />
+            <p className="text-lg font-thin">
+              Total Proposals : {job.proposals}
+            </p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <BookUser className="h-5" />
+            <p className="text-lg font-thin">
+              Creator :{" "}
+              <span className="text-blue-600 dark:text-blue-400 font-normal cursor-pointer">
+                {job.createdBy}
+              </span>
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <div className="flex gap-2 items-center">
+            <Calendar className="h-5" />
+            <p className="font-semibold">Posted Date : {job.createdAt}</p>
+          </div>
+          <div className="flex gap-4">
+            <SendProposalForm />
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/router";
 import getUser from "@/lib/hooks/getUser";
 import { Navbar } from "@/components/app/navbar";
 import { FreelancerNavbar } from "@/components/freelancer/freelancer-navbar";
 import { JobCard } from "@/components/freelancer/job-card";
+import { getAllActiveJobs } from "@/lib/hooks/getJobs";
+import { Job } from "@/lib/types";
 
 const Dashboard = () => {
   const router = useRouter();
   const { address } = useAccount();
+
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [filteredJobs, setFilteredJobs] = useState<Job[]>([]); // TO DO
 
   async function checkUser(address: string) {
     const res = await getUser(address);
@@ -25,21 +30,37 @@ const Dashboard = () => {
     }
   }
 
+  const fetchData = async () => {
+    try {
+      const data = await getAllActiveJobs();
+      setJobs(data);
+    } catch (error) {
+      console.error("Error fetching job data:", error);
+    }
+  };
+
   useEffect(() => {
     if (!address) {
       router.push("/");
     } else {
       checkUser(address);
+      fetchData();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address]);
+
   return (
     <>
       <div className="px-8 py-4">
         <FreelancerNavbar />
       </div>
       <div className="flex flex-col px-8 py-4">
-        <JobCard />
+        <div className="text-4xl px-8 py-8 font-semibold ">All Active Jobs</div>
+        {jobs.map((job: Job) => (
+          <>
+            <JobCard key={job.jobId} job={job} />
+          </>
+        ))}
       </div>
     </>
   );
