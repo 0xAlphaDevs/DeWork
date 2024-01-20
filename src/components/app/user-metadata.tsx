db73f818526e3777c51afbbe8ac0216873ea9bd0
0xyshv <writetoyshv@gmail.com>
2024-01-20 12:09:06 +0530

    chore: sync changes

--- src/components/app/user-metadata.tsx
+++ src/components/app/user-metadata.tsx
@@ -19,6 +19,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
 
 interface FormData {
   userName: string;
@@ -27,6 +28,8 @@ interface FormData {
 }
 
 export function UserMetadata() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [sendMetaData, setSendMetaData] = useState(false);
   const [formData, setFormData] = useState<FormData>({
     userName: "",
     userType: "",
@@ -40,6 +43,8 @@ export function UserMetadata() {
       userType: "",
       userLocation: "",
     });
+    setSendMetaData(false);
+    setIsLoading(false);
   }
 
   const constructMetaData = (
@@ -55,18 +60,20 @@ export function UserMetadata() {
     return newMetaData;
   };
 
-  async function sendMetaData() {
+  async function sendMetaDataForm() {
     try {
+      setIsLoading(true);
       const newMetaData = constructMetaData(
         formData.userName,
         formData.userType,
         formData.userLocation
       );
       console.log(" Data: ", newMetaData);
-
-      //   const result = await saveJobData(formData.title);
+      setIsLoading(false);
+      setSendMetaData(true);
     } catch (error) {
       console.error("Error submitting metaData:", error);
+      setIsLoading(false);
     }
   }
 
@@ -84,79 +91,99 @@ export function UserMetadata() {
     e.preventDefault();
     console.log("Registering...");
     console.log("Form Data: ", formData);
-    await sendMetaData();
+    await sendMetaDataForm();
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Register</Button>
+        <Button variant="outline" onClick={handleClick}>
+          Register
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Enter Details</DialogTitle>
-          <DialogDescription>
-            Register your details here to access the DeWork.
-          </DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleSubmitRequest}>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="userName" className="text-right">
-                Name
-              </Label>
-              <Input
-                id="userName"
-                value={formData.userName}
-                className="col-span-3"
-                onChange={handleChange}
-                name="userName"
-              />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="userType" className="text-right">
-                Username
-              </Label>
-              <Select
-                placeholder="Select a category"
-                onValueChange={(value: string) =>
-                  setFormData({
-                    ...formData,
-                    userType: value,
-                  })
-                }
-                className="border "
-                required
-                name="userType"
-              >
-                <SelectTrigger className="col-span-3">
-                  <SelectValue placeholder="Select a user type" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectItem value="health">Client</SelectItem>
-                    <SelectItem value="travel">Freelancer</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="userLocation" className="text-right">
-                Location
-              </Label>
-              <Input
-                id="userLocation"
-                value={formData.userLocation}
-                className="col-span-3"
-                onChange={handleChange}
-                name="userLocation"
-              />
-            </div>
+        {isLoading ? (
+          <div className="flex flex-col items-center justify-center h-40 gap-4">
+            {/* <Loader /> */}
+            <p>Creating record...</p>
           </div>
-          <DialogFooter>
-            <Button type="submit">Submit</Button>
-          </DialogFooter>
-        </form>
+        ) : (
+          <>
+            {!sendMetaData ? (
+              <>
+                <DialogHeader>
+                  <DialogTitle>Enter Details</DialogTitle>
+                  <DialogDescription>
+                    Register your details here to access the DeWork.
+                  </DialogDescription>
+                </DialogHeader>
+                <form onSubmit={handleSubmitRequest}>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="userName" className="text-right">
+                        Name
+                      </Label>
+                      <Input
+                        id="userName"
+                        value={formData.userName}
+                        className="col-span-3"
+                        onChange={handleChange}
+                        name="userName"
+                      />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="userType" className="text-right">
+                        Username
+                      </Label>
+                      <Select
+                        placeholder="Select a category"
+                        onValueChange={(value: string) =>
+                          setFormData({
+                            ...formData,
+                            userType: value,
+                          })
+                        }
+                        className="border "
+                        required
+                        name="userType"
+                      >
+                        <SelectTrigger className="col-span-3">
+                          <SelectValue placeholder="Select a user type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectItem value="health">Client</SelectItem>
+                            <SelectItem value="travel">Freelancer</SelectItem>
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="userLocation" className="text-right">
+                        Location
+                      </Label>
+                      <Input
+                        id="userLocation"
+                        value={formData.userLocation}
+                        className="col-span-3"
+                        onChange={handleChange}
+                        name="userLocation"
+                      />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <Button type="submit">Submit</Button>
+                  </DialogFooter>
+                </form>
+              </>
+            ) : (
+              <div className="flex flex-col gap-4 items-center">
+                <CheckCircledIcon className="w-20 h-20 text-green-500" />
+                <p>Registartion Successfully</p>
+              </div>
+            )}
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
